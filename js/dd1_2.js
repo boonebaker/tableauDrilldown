582cef937d2531b3501de427adb07ff5fbac6959
xps <xps@boone.boone>
2019-09-09 11:43:02 -0500

    cleanup

--- js/dd1_2.js
+++ js/dd1_2.js
@@ -14,7 +14,6 @@ function createParameterListener() {
         var p = t.find(p1 => p1.name === tableau.extensions.settings.get("param"));
         const pChanged = tableau.TableauEventType.ParameterChanged;
         p.addEventListener(pChanged, function(parameterEvent) {
-            console.log("showing " + id2);
             showid2();
         });
     });
